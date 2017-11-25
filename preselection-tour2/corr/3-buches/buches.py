ee80dcf7f722e1a75922bf8cde2d261a48075c4b
Mourad-NOUAILI <mouradnouaili1@gmail.com>
2017-11-25 21:22:56 +0100

    Delete buches.py

--- preselection-tour2/corr/3-buches/buches.py
+++
@@ -1,8 +0,0 @@
-n = input()
-tokens=[]
-tokens = input().split('X')
-tokens = filter(None, tokens)
-notEmptyTokens = [x for x in tokens if x != None]
-print (len(notEmptyTokens))
-for token in notEmptyTokens:
-    print (token)
